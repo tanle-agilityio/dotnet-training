2bcfd0c62c4a17ac9d46141dfc721a3d4186be7e
Tan Le <tan.le@dfinsolutions.com>
2023-11-14 17:15:33 +0700

    Persist and retrieve relational data by using Entity Framework Core

---
+++ Learning/mslearn-persist-data-ef-core/ContosoPizza/Controllers/CouponController.cs
@@ -0,0 +1,24 @@
+using ContosoPizza.Data;
+using ContosoPizza.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class CouponController : ControllerBase
+{
+    PromotionsContext _context;
+
+    public CouponController(PromotionsContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Coupon> Get()
+    {
+        return _context.Coupons.AsNoTracking().ToList();
+    }
+}
---
+++ Learning/mslearn-persist-data-ef-core/ContosoPizza/Services/PizzaService.cs
@@ -0,0 +1,81 @@
+using ContosoPizza.Data;
+using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Services;
+
+public class PizzaService
+{
+    private readonly PizzaContext _context;
+    public PizzaService(PizzaContext context)
+    {
+        this._context = context;
+    }
+
+    public IEnumerable<Pizza> GetAll()
+    {
+        return _context.Pizzas.AsNoTracking().ToList();
+    }
+
+    public Pizza? GetById(int id)
+    {
+        return _context.Pizzas
+            .Include(p => p.Toppings)
+            .Include(p => p.Sauce)
+            .AsNoTracking()
+            .SingleOrDefault(p => p.Id == id);
+    }
+
+    public Pizza? Create(Pizza newPizza)
+    {
+        _context.Pizzas.Add(newPizza);
+        _context.SaveChanges();
+
+        return newPizza;
+    }
+
+    public void AddTopping(int PizzaId, int ToppingId)
+    {
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var toppingToAdd = _context.Toppings.Find(ToppingId);
+
+        if (pizzaToUpdate is null || toppingToAdd is null)
+        {
+            throw new InvalidOperationException("Pizza or topping does not exist");
+        }
+
+        if (pizzaToUpdate.Toppings is null) 
+        {
+            pizzaToUpdate.Toppings = new List<Topping>();
+        }
+
+        pizzaToUpdate.Toppings.Add(toppingToAdd);
+
+        _context.SaveChanges();
+    }
+
+    public void UpdateSauce(int PizzaId, int SauceId)
+    {
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var sauceToAdd = _context.Sauces.Find(SauceId);
+
+        if (pizzaToUpdate is null || sauceToAdd is null)
+        {
+            throw new InvalidOperationException("Pizza or sauce does not exist");
+        }
+
+        pizzaToUpdate.Sauce = sauceToAdd;
+        _context.SaveChanges();
+    }
+
+    public void DeleteById(int id)
+    {
+        var pizzaToDelete = _context.Pizzas.Find(id);
+
+        if (pizzaToDelete is not null)
+        {
+            _context.Pizzas.Remove(pizzaToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
