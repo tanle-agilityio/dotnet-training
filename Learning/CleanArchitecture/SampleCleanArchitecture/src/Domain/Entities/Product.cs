c2c04c32efe0fa1134b027ebcaf3e937060f2090
Tan Le <tan.le@dfinsolutions.com>
2023-12-08 10:44:39 +0700

    add migrations for product entity

---
+++ Learning/CleanArchitecture/SampleCleanArchitecture/src/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleCleanArchitecture.Domain.Entities;
+public class Product : BaseAuditableEntity
+{
+    public string? ProductName { get; set; }
+    [Column(TypeName = "decimal(18,4)")]
+    public decimal ProductPrice { get; set; }
+
+}
---
+++ Learning/CleanArchitecture/SampleCleanArchitecture/src/Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SampleCleanArchitecture.Domain.Entities;
+
+namespace SampleCleanArchitecture.Infrastructure.Data.Configurations;
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(p => p.ProductName)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
